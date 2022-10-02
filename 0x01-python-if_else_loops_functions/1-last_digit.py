f21b056d17a0777171c68985cb88bd666a03377c
abenezerapy <106971846+abenezerapy@users.noreply.github.com>
2022-10-02 15:57:35 +0300

    Working on last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,3 +10,5 @@ elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 5 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} and not 0")
+else:
+    print("Invalid!!")
